b7cb743d815b3305a21e12972110e5278ee7f203
sypna2 <dany.altshul@yandex.ru>
2026-05-13 23:54:25 +0300

    added constraints file

---
+++ constraints.py
@@ -0,0 +1,2 @@
+from pathlib import Path
+TESTING_FILES_DIR = Path("files_for_tests")
--- tests/tests_upload.py
+++ tests/tests_upload.py
@@ -1,10 +1,11 @@
 from pathlib import Path
 from fastapi.testclient import TestClient
 from app.main import app
+from constraints import TESTING_FILES_DIR
+
 
 client = TestClient(app)
 
-TESTING_FILES_DIR = Path("files_for_tests")
 
 def upload_file(filename):
     file_path = TESTING_FILES_DIR/filename
@@ -14,35 +15,31 @@ def upload_file(filename):
             "/upload-grades",
             files={"file": (filename, file, "text/csv")},
         )
+    
 def test_missing_columns_file():
     response = upload_file("missing_columns.csv")
 
     assert response.status_code == 422
-
 
 def test_invalid_grade_range_file():
     response = upload_file("invalid_grade_range.csv")
 
     assert response.status_code == 422
 
-
 def test_invalid_grade_type_file():
     response = upload_file("invalid_grade_type.csv")
 
     assert response.status_code == 422
-
 
 def test_missing_values_file():
     response = upload_file("missing_values.csv")
 
     assert response.status_code == 422
 
-
 def test_empty_grades_file():
     response = upload_file("empty_grades.csv")
 
     assert response.status_code in [400, 422]
-
 
 def test_wrong_extension_file():
     response = upload_file("test_file.cvs")
